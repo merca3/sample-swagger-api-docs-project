eb277b32354b478020bcee1a38cddab9c20be762
Katrina Merca <78380665+merca3@users.noreply.github.com>
2025-11-21 09:34:28 +0200

    Update app to serve and test docs with localhost

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
-import express from 'express';
-import patientRouter from './routes/patientRouter.js';
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import yaml from "js-yaml";
+import patientRouter from "./routes/patientRouter.js";
 
-const app = express(); 
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('API is running. Navigate to /api/v1/patients to interact with the API.');
+const openapi = yaml.load(
+  fs.readFileSync("nodejs-server-server-generated/api/openapi.yaml", "utf8")
+);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapi));
+
+app.get("/", (req, res) => {
+  res.send(
+    "API is running. Navigate to /api/v1/patients to interact with the API."
+  );
 });
 
-app.use('/api/v1', patientRouter);
+app.use("/api/v1", patientRouter);
 
 app.use((error, req, res, next) => {
-    console.error(error);
-    if(error.message === 'notfound'){
-        res.status(404).json({error: "No patient found with the provided ID."});
-    } else {
-        res.status(500).json({
-            error: "An internal server error occurred."
-        });
-    }
+  console.error(error);
+  if (error.message === "notfound") {
+    res.status(404).json({ error: "No patient found with the provided ID." });
+  } else {
+    res.status(500).json({
+      error: "An internal server error occurred.",
+    });
+  }
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
